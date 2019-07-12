ef60768a660a1b771fbac135744084e768207b63
mmoyab <mmoyab@gmail.com>
2019-07-12 17:02:33 +0200

    CRUD Logic done

--- src/models/cast.js
+++ src/models/cast.js
@@ -4,10 +4,10 @@ const { Schema } = mongoose;
 const CastMemberSchema = new Schema({
     first_name: {type: String, required:true},
     last_name: {type: String, required:true},
-    dob: { type: Date, required:true},
-    phoneNumber: { type: Number, required:true},
-    countryResidency: {type: String, required:true},
-    email: { type:String, required:true}
+    // dob: { type: String, required:false},
+    // phoneNumber: { type: Number, required:true},
+    // countryResidency: {type: String, required:true},
+    // email: { type:String, required:true}
 });
 
 module.exports = mongoose.model('Cast', CastMemberSchema);
--- src/routes/cast.routes.js
+++ src/routes/cast.routes.js
@@ -3,14 +3,37 @@ const router = express.Router();
 
 const Cast = require('../models/cast');
 
-router.get('/', (req, res) => {
-   Cast.find(function (err, task) {
-       console.log(Cast);
+router.get('/', async (req, res) => {
+   const CastMembers = await Cast.find();
+       res.json(CastMembers);
    });
 
-    res.json({
-        status:'API works'
-    });
+router.get('/:id', async (req, res) => {
+   const CastMember = await Cast.findById(req.params.id);
+   res.json(CastMember);
 });
+
+router.post('/', async (req, res) => {
+    const { first_name, last_name} = req.body;
+    const CastMember = new Cast({first_name,last_name});
+    await CastMember.save();
+    console.log(CastMember);
+    res.json({status: 'Cast Member Saved'});
+});
+
+router.put('/:id' , async (req, res) => {
+    const { first_name, last_name} = req.body;
+    const newCastMember = { first_name, last_name};
+    await Cast.findByIdAndUpdate(req.params.id, newCastMember);
+    console.log(req.param.id);
+    res.json({status: 'Cast Member info Updated'});
+});
+
+router.delete('/:id', async (req, res) => {
+    await Cast.findByIdAndRemove(req.params.id);
+    res.json({status: 'Cast member info deleted'});
+});
+
+
 
 module.exports = router;
