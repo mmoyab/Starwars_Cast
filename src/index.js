381b045c166beaef9101572de300dca648f267e6
mmoyab <mmoyab@gmail.com>
2019-07-12 11:39:55 +0200

    Setting up Server

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000, () => {
-    console.log('Server listening on port 3000');
+// Settings
+
+// Setting for app deployment (Toma el puerto asignado por el servicio, o el puerto por defecto)
+app.set('port', process.env.PORT || 3000);
+
+// Middelwares
+
+// Routes
+
+// Static Files
+
+//Starting Server
+
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
 });
